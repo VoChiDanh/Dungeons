291954302db5cc913c9043cd5f4cff3dadf910cf
Danh <vochidanh.hs.c@gmail.com>
2024-09-03 22:38:34 +0700

    1.5.0-SNAPSHOT
    - (Other changes above)
    - Hopeful fix lag when generate new world
    - Fix if mob wasn't kill by player, it didn't count
    - Add papi ```%dungeons_party%```
    - Fix party disband message doesn't send to everyone
    - Party System p3 (Almost done, not tested yet)
    - Party System p4+5 (Add /party accept <id>, fix bug, fix message)
    - Fix party when end dungeon
    - Backup old file to generate new !
    - Fix rewards doesn't give on complete dungeons
    - Fix duplication req items
    - Fix world doesn't unload when stop server
    - Fix creeper (?)

--- src/main/java/net/danh/dungeons/Listeners/VanillaMobs.java
+++ src/main/java/net/danh/dungeons/Listeners/VanillaMobs.java
@@ -6,11 +6,13 @@ import net.danh.dungeons.Dungeons;
 import net.danh.dungeons.Party.PartyManager;
 import net.danh.dungeons.Resources.Number;
 import org.bukkit.World;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
@@ -18,6 +20,67 @@ import java.util.HashMap;
 public class VanillaMobs implements Listener {
 
     public static HashMap<String, Integer> kill = new HashMap<>();
+
+    @EventHandler
+    public void onEntityExplosion(@NotNull EntityExplodeEvent e) {
+        Entity mob = e.getEntity();
+        String vanillaMobs = e.getEntityType().toString();
+        World world = mob.getWorld();
+        for (Player killer : world.getPlayers()) {
+            killer = PartyManager.getPlayer(killer);
+            if (StageManager.inDungeon(killer)) {
+                String dungeonID = StageManager.getPlayerDungeon(killer);
+                DungeonManager dungeonManager = new DungeonManager(dungeonID);
+                int stageNumber = StageManager.getStageNumber(killer);
+                String stageID = dungeonManager.getConfig().getString("stages.stage_" + stageNumber + ".id");
+                if (stageID != null) {
+                    if (stageID.equalsIgnoreCase("v_kill_mob")) {
+                        String type = dungeonManager.getConfig().getString("stages.stage_" + stageNumber + ".type");
+                        if (type != null && type.equalsIgnoreCase(vanillaMobs)) {
+                            int amount = Number.getInteger(dungeonManager.getConfig().getString("stages.stage_" + stageNumber + ".amount"));
+                            if (kill.containsKey(killer.getName() + "_" + dungeonID)) {
+                                if (amount > kill.get(killer.getName() + "_" + dungeonID)) {
+                                    kill.replace(killer.getName() + "_" + dungeonID, kill.get(killer.getName() + "_" + dungeonID) + 1);
+                                    if (amount <= kill.get(killer.getName() + "_" + dungeonID)) {
+                                        kill.remove(killer.getName() + "_" + dungeonID);
+                                        StageManager.nextStage(killer);
+                                    }
+                                }
+                            } else {
+                                kill.put(killer.getName() + "_" + dungeonID, 1);
+                                if (amount <= kill.get(killer.getName() + "_" + dungeonID)) {
+                                    kill.remove(killer.getName() + "_" + dungeonID);
+                                    StageManager.nextStage(killer);
+                                }
+                            }
+                        }
+                    } else if (stageID.equalsIgnoreCase("mm_kill_mob")) {
+                        if (Dungeons.getMythicAPI().isMythicMob(e.getEntity())) {
+                            String type = dungeonManager.getConfig().getString("stages.stage_" + stageNumber + ".type");
+                            if (type != null && type.equalsIgnoreCase(Dungeons.getMythicAPI().getInternalName(e.getEntity()))) {
+                                int amount = Number.getInteger(dungeonManager.getConfig().getString("stages.stage_" + stageNumber + ".amount"));
+                                if (kill.containsKey(killer.getName() + "_" + dungeonID)) {
+                                    if (amount > kill.get(killer.getName() + "_" + dungeonID)) {
+                                        kill.replace(killer.getName() + "_" + dungeonID, kill.get(killer.getName() + "_" + dungeonID) + 1);
+                                        if (amount <= kill.get(killer.getName() + "_" + dungeonID)) {
+                                            kill.remove(killer.getName() + "_" + dungeonID);
+                                            StageManager.nextStage(killer);
+                                        }
+                                    }
+                                } else {
+                                    kill.put(killer.getName() + "_" + dungeonID, 1);
+                                    if (amount <= kill.get(killer.getName() + "_" + dungeonID)) {
+                                        kill.remove(killer.getName() + "_" + dungeonID);
+                                        StageManager.nextStage(killer);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     @EventHandler(ignoreCancelled = true)
     public void onVanillaKill(@NotNull EntityDeathEvent e) {
