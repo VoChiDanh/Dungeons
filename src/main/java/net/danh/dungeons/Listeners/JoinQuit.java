02e126daeb090ef92856c881579d8b8e8aa4e128
Danh <vochidanh.hs.c@gmail.com>
2024-09-04 20:09:51 +0700

    1.5.0-SNAPSHOT
    - (Other changes above)
    - Fix party issues when quit in dungeon
    - Fix check points (x2)
    - Fix party issues

--- src/main/java/net/danh/dungeons/Listeners/JoinQuit.java
+++ src/main/java/net/danh/dungeons/Listeners/JoinQuit.java
@@ -2,11 +2,6 @@ package net.danh.dungeons.Listeners;
 
 import net.danh.dungeons.Dungeon.StageManager;
 import net.danh.dungeons.Party.PartyManager;
-import net.xconfig.bukkit.model.SimpleConfigurationManager;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.World;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -19,29 +14,11 @@ public class JoinQuit implements Listener {
     public void onQuit(@NotNull PlayerQuitEvent e) {
         Player p = e.getPlayer();
         if (StageManager.inDungeon(p)) {
-            if (StageManager.inDungeon(p)) {
-                if (!PartyManager.inParty(p))
-                    StageManager.endDungeon(p, false, true);
-                else {
-                    String dungeonID = StageManager.getPlayerDungeon(p);
-                    FileConfiguration config = SimpleConfigurationManager.get().get("Dungeons/" + dungeonID + ".yml");
-                    String locationComplete = config.getString("location.complete");
-                    if (locationComplete != null) {
-                        World world = Bukkit.getWorld(locationComplete.split(";")[0]);
-                        Location rLocation = StageManager.getLocation(locationComplete.replace(locationComplete.split(";")[0] + ";", ""), world);
-                        if (PartyManager.isPartyLeader(p)) {
-                            PartyManager.getMembers(p).forEach(player -> player.teleport(rLocation));
-                            Bukkit.getServer().unloadWorld(config.getString("world") + "_" + p.getName() + "_" + dungeonID, false);
-                            StageManager.delete(config.getString("world") + "_" + p.getName() + "_" + dungeonID);
-                            StageManager.endPartyDungeon(p, false, true);
-                        }
-                    }
-                }
-            }
-        }
-        if (PartyManager.inParty(p)) {
-            if (PartyManager.isPartyLeader(p))
+            if (!PartyManager.inParty(p))
+                StageManager.endDungeon(p, false, true);
+            else {
                 PartyManager.disbandParty(p);
+            }
         }
     }
 }
